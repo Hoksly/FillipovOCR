0f6d991d2e681db1e82d5dcf4f579ba55b07da92
hoksly <realhoksly@gmail.com>
2022-11-15 22:14:58 +0200

    Assembler prototype added

---
+++ assembling/assemble.py
@@ -0,0 +1,14 @@
+from Collection import RawImage
+
+class Assembler:
+    def __init__(self):
+        pass
+
+    def assemble(self, rawImages: list):
+        """
+
+        :param rawImages: list of RawImage
+        :return: str differential equation string
+        """
+        return "None"
+
