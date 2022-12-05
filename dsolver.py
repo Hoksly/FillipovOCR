6f242770f4371af4a02f75d1355ef5afafcd81d6
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2022-12-05 21:06:13 +0200

    Added infinite loop indicator

--- dsolver.py
+++ dsolver.py
@@ -1,3 +1,5 @@
+import signal
+
 from sympy import *
 
 
@@ -21,22 +23,79 @@ class DSolver:
         """
         to_solve = DSolver.recognize_symbols(equation)
         try:
-            solution = DSolver.solve_equation(to_solve)
+            solution = DSolver.solve_from_string(to_solve)
             return latex(solution)
-        except ValueError:
+        except Exception as e:
+            print(e)
             return ""
-
 
     @staticmethod
     def recognize_symbols(equation: str):
-        formatted = equation.replace("y'", "diff(y, x)")
+        formatted = equation.replace(" ", "")
+        formatted = formatted.replace("dx", "1")
+        formatted = formatted.replace("dy", "y'")
+        formatted = DSolver.insert_skipped_multiply(formatted)
+        formatted = formatted.replace("^", "**")
+        formatted = formatted.replace("e", "E")
+        formatted = formatted.replace("y'", "(diff(y, x))")
         formatted = formatted.replace("y", "y(x)")
         return formatted
 
     @staticmethod
-    def solve_equation(equation):
+    def solve_from_string(equation):
         parts = equation.split("=")
-        assert len(parts) == 2
+        if len(parts) != 2:
+            return ""
         deq = Eq(parse_expr(parts[0]), parse_expr(parts[1]))
+        DSolver.called_solver = False
+        result = DSolver.solve_equation(deq)
+        if result is None and not DSolver.called_solver:
+            result = dsolve(deq)
+        return result
+
+    called_solver = True
+
+    @staticmethod
+    def time_break(func):
+        def wrapper(*args, **kwargs):
+            try:
+                signal.alarm(10)
+                DSolver.called_solver = True
+                result = func(*args, **kwargs)
+                signal.alarm(0)
+                return result
+            except Exception:
+                return None
+
+        return wrapper
+
+    @staticmethod
+    @time_break
+    def solve_equation(deq: Eq):
         return dsolve(deq)
 
+    @staticmethod
+    def insert_skipped_multiply(equation: str):
+        s = ""
+        a = len(equation) - 1
+        for i in range(0, a):
+            s += equation[i]
+            if equation[i + 1] in "exy(" and equation[i] in '012345689abcABCDxy':
+                s += '*'
+            elif equation[i] in "exy)'" and equation[i + 1] in '012345689abcABCDxy':
+                s += '*'
+        s += equation[a]
+        return s
+
+    @staticmethod
+    def to_derivative_from(equation: str):
+        s = ""
+        a = len(equation) - 1
+        for i in range(0, a):
+            s += equation[i]
+            if equation[i + 1] in "exy(" and equation[i] in '012345689abcABCDxy':
+                s += '*'
+            elif equation[i] in "exy)'" and equation[i + 1] in '012345689abcABCDxy':
+                s += '*'
+        s += equation[a]
+        return s
--- test_solver.py
+++ test_solver.py
@@ -10,10 +10,29 @@ class TestSolver(TestCase):
         t = symbols('t')
         z = Function('z')
         deq = Eq(parse_expr("diff(z(t), t)"), parse_expr("z(t)"))
-        xsoln = dsolve(deq, z(t))
-        print(latex(xsoln))
+        solution = dsolve(deq, z(t))
+        print(latex(solution))
+
     def test_simple(self):
         print(DSolver.solve("y'=y"))
 
     def test_complex(self):
         print(DSolver.solve("y'+y*tan(x)=1/cos(x)"))
+
+    def test_real_examples9(self):
+        print(DSolver.solve("y' = x - e^y"))
+
+    def test_real_examples25(self):
+        print(DSolver.solve("y'=ax^2 + be^x"))
+
+    def test_real_examples50(self):
+        print(DSolver.solve("y' = Cx + C^3"))
+
+    def test_real_examples64(self):
+        print(DSolver.solve("(x+2y)y' = 1"))
+
+    def test_real_examples103(self):
+        print(DSolver.solve(" (x y +e^x) dx - xdy = y"))
+
+    def test_infinite(self):
+        print(DSolver.solve("y'^4=y"))
